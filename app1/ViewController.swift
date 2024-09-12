85b3d79fdb8bd2878e61008c09533058b7482abb
maaouikarima <maaouika07@gmail.com>
2024-09-12 16:02:44 +0100

    first commit

--- app1/ViewController.swift
+++ app1/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input1: UITextField!
+    @IBOutlet weak var input2: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func div(_ sender: Any) {
+        if let nb = Double(input1.text ?? "0" ),
+           let nb1 = Double(input2.text ?? "0"){
+            let sum = nb / nb1
+            label.text = String(sum)
+        }
+    }
+    @IBAction func moins(_ sender: Any) {
+        if let nb = Double(input1.text ?? "0" ),
+           let nb1 = Double(input2.text ?? "0"){
+            let sum = nb - nb1
+            label.text = String(sum)
+        }
+        
+    }
+    @IBAction func plus(_ sender: Any) {
+        if let nb = Double(input1.text ?? "0" ),
+           let nb1 = Double(input2.text ?? "0"){
+            let sum = nb + nb1
+            label.text = String(sum)
+        }
+    }
+    @IBAction func fois(_ sender: Any) {
+        if let nb = Double(input1.text ?? "0" ),
+           let nb1 = Double(input2.text ?? "0"){
+            let sum = nb * nb1
+            label.text = String(sum)
+        }
+    }
+    
 }
 
